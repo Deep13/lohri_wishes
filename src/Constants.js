6b48a1b613d5fc2c39a777713ddfca1fa29c8924
Deep13 <deep.deepak30@gmail.com>
2024-05-18 22:53:43 +0530

    initial commit

---
+++ src/Constants.js
@@ -0,0 +1,205 @@
+export const day_title = 'Happy Lohri';
+export const banner_key1 = 'ca-app-pub-4717579333914549/2682432065';
+export const banner_key2 = 'ca-app-pub-4717579333914549/3803942042';
+export const banner_key3 = 'ca-app-pub-4717579333914549/1058359207';
+export const interstitial_key1 = 'ca-app-pub-4717579333914549/3383116121';
+export const interstitial_key2 = 'ca-app-pub-4717579333914549/3564554304';
+export const interstitial_key3 = 'ca-app-pub-4717579333914549/9938390967';
+export const language = ['English', 'Hindi', 'Punjabi'];
+export const textData = {
+  English: {
+    "wishes": [
+      {
+        id: '001',
+        text:
+          'May this festival bring you and your family happiness and prosperity. May it fill you with zeal and verve. Happy Lohri to one and all!',
+      },
+      {
+        id: '002',
+        text:
+          'Spread the message of happiness and peace of Lohri all around. Hope your day is full of excitement. I wish you a Happy Lohri!',
+      },
+      {
+        id: '003',
+        text:
+          'May your year be just as colourful and joyful as the festival of Lohri. Wish you a very happy Lohri!',
+      },
+      {
+        id: '004',
+        text:
+          'Hope the deliciousness of taste of makke ki roti and sarson ka sag bring you lots of joy and prosperity on this auspicious festival. Happy Lohri!',
+      },
+      {
+        id: '005',
+        text:
+          'Hope this celebration of the bonfire brings you warmth and the sweetness of rewari remain with you forever. A very happy Lohri!',
+      },
+      {
+        id: '006',
+        text:
+          'Wishing a very Happy Lohri to you and your family. May the Lohri fire burn away all the sadness out of your life and bring you joy, happiness and love.',
+      },
+      {
+        id: '007',
+        text:
+          'Wishing this harvest season brings a smile to light up your face and you enjoy the year with a bang! Happy Lohri!',
+      },
+      {
+        id: '008',
+        text:
+          'May this Lohri bring lots of laughter, happiness, prosperity and success to your home. Happy Lohri!',
+      },
+      {
+        id: '009',
+        text:
+          'Happy Lohri to you all! Hope the fire of the holy bonfire provides warmth and divine guidance this Lohri.',
+      },
+      {
+        id: '010',
+        text:
+          'Have a rocking time! Balle Balle! Lohri is here, dhol bajao, nachcho gao! Happy Lohri!',
+      },
+      {
+        id: '011',
+        text:
+          "Here's wishing you the warmth of the bonfire and the sweetens of til and gur — a very Happy Lohri to your family, and my dear friend to you.",
+      },
+      {
+        id: '012',
+        text:
+          'May the sacred fire of Lohri shower you with good luck, good health and the goodness of all the good things on earth. A very happy Lohri to you.',
+      },
+    ]
+  }
+  ,
+  Hindi: {
+    "wishes": [
+      {
+        id: '001',
+        text:
+          'सर्दी की थरथराहट में; मूंगफली, रेवड़ी और गुड़ की मिठास के साथ; लोहड़ी मुबारक हो प्यार, दोस्ती और हर रिश्ते की गर्माहट के साथ।लोहड़ी की शुभकामनाएं!',
+      },
+      {
+        id: '002',
+        text:
+          'फिर आ गई भंगड़े की बारी; लोहड़ी मनाने की करो तैयारी; आग के पास सब आओ; सुंदर-मुंदरिये जोर से गाओ; लोहड़ी की शुभकामनाएं.',
+      },
+      {
+        id: '003',
+        text:
+          'पॉपकॉर्न की खुशबु, मूंगफली रेबड़ी की बहार, थोड़ी सी मस्ती, अपनों का प्यार.. आपको मुबारक हो लोहरी का त्यौहार हैप्पी लोहरी 2021..',
+      },
+      {
+        id: '004',
+        text:
+          'दिल की ख़ुशी और अपनों का प्यार; मुबारक हो आपको लोहरी का त्यौहार || लोहरी की शुभकामनाएँ !',
+      },
+      {
+        id: '005',
+        text:
+          'मीठे गुड में मिल गया तिल, उड़ीं पतंग और खिल गया दिल, आपके जीवन में आये हर दिन सुख और शांति! मुबारक हो आपको लोहड़ी का त्यौहार..',
+      },
+      {
+        id: '006',
+        text:
+          'जैसे जैसे लोहड़ी की आग तेज हो वैसे वैसे हमारे दुखों का अंत हो, लोहड़ी का प्रकाश आपकी जिन्दगी को प्रकाशमय कर दे! हैप्पी लोहड़ी',
+      },
+      {
+        id: '007',
+        text:
+          'फिर आगया मौसम मक्की दी रोटी और सरसों दे साग का,सबको मुबारक हो लोहरी का ये त्योहार |',
+      },
+      {
+        id: '008',
+        text:
+          'आपको और आपके परिवार को लोहड़ी की लाख-लाख बधाइयाँ, रब करे अप के जीवन में इन्हों खुशियों की बारिश होवे, आपको लोहरी उत्सव की बधाई हो!!',
+      },
+      {
+        id: '009',
+        text:
+          'तन में मस्ती, मन में उमंग, चलो आकाश में डालें रंग, हो जायें सब संग-संग, उड़ायें पतंग. हैप्पी लोहरी।।',
+      },
+      {
+        id: '010',
+        text:
+          'लोहड़ी की आग आपके दुखों को जला दे; और आग की रौशनी आपकी ज़िंदगी में उजाला भर दे। — लोहड़ी की शुभकामनाएं',
+      },
+      {
+        id: '011',
+        text:
+          'चाँद को चांदनी मुबारक,दोस्त को दोस्ती मुबारक,मुझको आप मुबारक,और मेरी तरफ़ से आपको लोहड़ी मुबारक',
+      },
+      {
+        id: '012',
+        text:
+          'मीठे गुड में मिल गया तिल,उड़ीं पतंग और खिल गया दिल,आपके जीवन में आये हर दिन सुख और शांति, लोहड़ी मुबारक!!',
+      },
+    ]
+  },
+  Punjabi: {
+    "wishes": [
+      {
+        id: '001',
+        text:
+          'ਜਦੋ ਇਹ ਆਉਂਦੀ ਏ ਲੋਹੜੀ, ਬੜਾ ਜੀਅ ਲਾਉਂਦੀ ਏ ਲੋਹੜੀ, ਇਹ ਲਾਡ ਮਲਾਰਾਂ ਦੀ ਲੋਹੜੀ, ਮੁਹੱਬਤ ਪਿਆਰਾਂ ਦੀ ਲੋਹੜੀ, ਹੈਪੀ ਲੋਹੜੀ',
+      },
+      {
+        id: '002',
+        text:
+          'ਮੇਰੇ ਵੱਲੋਂ ਤੁਹਾਨੂੰ ਅਤੇ ਤੁਹਾਡੇ ਸਾਰੇ ਪਰਿਵਾਰ ਨੂੰ ਲੋਹੜੀ ਦੇ ਤਿਉਹਾਰ ਦੀਆਂ ਲੱਖ-ਲੱਖ ਮੁਬਾਰਕਾਂ!',
+      },
+      {
+        id: '003',
+        text:
+          'ਉਮੀਦ ਹੈ ਕਿ ਦੈਵੀ ਕ੍ਰਿਪਾ ਤੁਹਾਡੇ ਦਿਲ ਨੂੰ ਖੁਸ਼ ਕਰਦੀ ਹੈ ਅਤੇਇਸ ਸ਼ੁਭ ਮੌਕੇ ਤੇ ਤੁਹਾਡੀਆਂ ਸਾਰੀਆਂ ਇੱਛਾਵਾਂ ਨੂੰ ਪੂਰਾ ਕਰਦਾ ਹੈ.ਖੁਸ਼ੀ ਲੋਹੜੀ',
+      },
+      {
+        id: '004',
+        text:
+          'ਸੁੰਦਰ ਮੁੰਦਰੀਏ ਹੋ, ਤੇਰਾ ਕੌਣ ਵਿਚਾਰਾ ਹੋ, ਦੁੱਲਾ ਭੱਟੀ ਵਾਲਾ ਹੋ, ਦੁੱਲੇ ਦੀ ਧੀ ਵਿਆਹੀ ਹੋ, ਬੱਸ ਬੱਸ ਆ ਲੈ 1 ਰੁਪਇਆ ਬਾਕੀ ਲੋਹੜੀ ਤੇ ਆਈਂ.. ਹੈਪੀ ਲੋਹੜੀ',
+      },
+      {
+        id: '005',
+        text:
+          'ਹੌਲੀ ਹੌਲੀ ਸਾਰੇ ਛੱਡ ਗਏ, ਨਵੀਂ ਉਮਰ ਦੀ ਪੌੜੀ ਮਿੱਠੀਆਂ ਯਾਦਾਂ ਸਾਂਭ ਕੇ ਰੱਖੀਏ, ਭੁੱਲ ਜਾਈਏ ਗੱਲ ਕਹੀ ਕੌੜੀ, ਗੱਚਕ ਮੂੰਗਫਲੀ ਖਾ ਖਾ ਰੱਜੀਏ, ਤੇ ਚੱਬ ਚੱਬ ਰੱਜੀਏ ਰਿਉੜ! ਹੈਪੀ ਲੋਹੜੀ',
+      },
+      {
+        id: '006',
+        text:
+          ' ਭੰਗੜਾ ਗਿੱਧਾ ਪਾਉਣ ਦੀ ਵਾਰੀ ਏ, ਸਭ ਨੇ ਖਿੱਚੀ ਲੋਹੜੀ ਮਨਾਉਣ ਦੀ ਤਿਆਰੀ ਏ...',
+      },
+      {
+        id: '007',
+        text:
+          'ਮੇਰੇ ਵੱਲੋਂ ਤੁਹਾਨੂੰ ਅਤੇ ਤੁਹਾਡੇ ਸਾਰੇ ਪਰਿਵਾਰ ਨੂੰ, ਲੋਹੜੀ ਦੇ ਤਿਉਹਾਰ ਦੀਆਂ ਲੱਖ-ਲੱਖ ਮੁਬਾਰਕਾਂ!',
+      },
+      {
+        id: '008',
+        text:
+          'ਅਸੀਂ ਤੁਹਾਡੇ ਦਿਲ ਵਿੱਚ ਰਹਿਣੇ ਆਂ, ਏਸੇ ਕਰਕੇ ਤਾਂ ਸਾਰੇ ਗਮ ਸਹਿਣੇ ਆਂ, ਪਰ ਫੇਰ ਵੀ ਸਾਰਿਆਂ ਤੋਂ ਪਹਿਲਾਂ ਤੁਹਾਨੂੰ ਹੈਪੀ ਲੋਹੜੀ ਕਹਿਨੇ ਆਂ...ਹੈਪੀ ਲੋਹੜੀ',
+      },
+      {
+        id: '009',
+        text:
+          'ਇੱਛਾ ਹੈ ਕਿ ਬੌਨ ਅੱਗ, ਮਿੱਠੀਤਾ ਦਾ ਨਿੱਘਲੋਹੜੀ ਵਿਖੇ ਗੁਰ ਅਤੇ ਰਾਏਰੀ ਦਾ ਸਦਾ ਲਈ ਤੁਹਾਡੇ ਨਾਲ ਰਹੇਗਾ. ',
+      },
+      {
+        id: '010',
+        text:
+          'ਪ੍ਰਮਾਤਮਾ ਦੀਆਂ ਬੇਅੰਤ ਬਖਸ਼ਿਸ਼ਾਂ ਦੀ ਕਾਮਨਾ ਕਰਨਾ ਤੁਹਾਡੀ ਜਿੰਦਗੀ ਨੂੰ ਖੁਸ਼ਹਾਲੀ ਅਤੇ ਖੁਸ਼ਹਾਲ ਹੈਰਾਨੀ ਨਾਲ ਭਰਪੂਰ ਬਣਾਉਂਦਾ ਹੈ ਇਸ ਲੋਹੜੀ ਅਤੇ ਹਮੇਸ਼ਾਂ. ਖੁਸ਼ਹਾਲ ਲੋਹੜੀ ਹੋਵੇ',
+      },
+      {
+        id: '011',
+        text:
+          'ਲੋਹੜੀ ਦੀ ਅੱਗ ਵਿਚ ਰਹਿੰਦਾ ਹੈ. ਆਓ ਆਪਾਂ ਇਕ ਵਾਰ ਆਪਣੇ ਪਿਆਰਿਆਂ ਨਾਲ ਵਧੇਰੇ ਖੁਸ਼ਹਾਲ ਵਾਰਾਂ ਦੀ ਉਮੀਦ ਕਰੀਏ. ਤੁਹਾਨੂੰ ਲੋਹੜੀ ਮੁਬਾਰਕ.',
+      },
+      {
+        id: '012',
+        text:
+          'ਪ੍ਰਮਾਤਮਾ ਸਾਡੀ ਜਿੰਦਗੀ ਨੂੰ ਲੋਹੜੀ ਦੀ ਅੱਗ ਵਾਂਗ ਚਮਕਦਾਰ ਕਰੇ ਅਤੇ ਖੁਸ਼ਹਾਲੀ ਅਤੇ ਖੁਸ਼ਹਾਲੀ ਦੇ ਨਾਲ ਵਰ੍ਹੇ. ਮੁਬਾਰਕ ਲੋਹੜੀ',
+      },
+    ]
+  }
+
+
+};
